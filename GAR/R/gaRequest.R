879919c572a88aa9b57786d3caa1f5190dcddd5e
andrewgeisler <andrew.geisler@gmail.com>
2015-03-01 21:15:50 -0600

    added new function files and updated manual documenation for version 1.1

--- GAR/R/gaRequest.R
+++ GAR/R/gaRequest.R
@@ -1,53 +1,87 @@
 gaRequest <-
 function(id, dimensions, metrics, start, end, token, sort=NA, max=NA, filters=NA) {
-  
-                      ##CREATE LIST OF QUERY PARAMETERS
-                      queryList <- list(
-                                    'ids'=id,
-                                    'dimensions'=dimensions,
-                                    'metrics'=metrics,
-                                    'start-date'=start,
-                                    'end-date'=end,
-                                    'sort'=sort,
-                                    'max-results'=max,
-                                    'filters'=filters,
-                                    'access_token'=token
-                                  )
-                      
-                      ##REMOVE ANY UNUSED PARAMETERS
-                      queryList <- queryList[!is.na(queryList)]
-                      
-                      ##BUILD QUERY AND FETCH RESULTS
-                      response <- GET("https://www.googleapis.com/analytics/v3/data/ga",
-                          query = queryList
-                          )
-                      
-                      #PARSE JSON RESPONSE FROM GA
-                      response <- jsonlite::toJSON(content(response))
-                      response <- jsonlite::fromJSON(response, flatten=TRUE)
-                      
-                      #CREATE DATAFRAME FOR RESULT ROWS
-                      df <- data.frame(response$rows, stringsAsFactors=FALSE)
-                      
-                      #RENAME DF COLUMN NAME
-                      colnames(df) <- unlist(response$columnHeaders$name)
-                      
-                      #FORMAT METRIC DATA AS NUMERIC
-                      for (i in 1:length(response$query$metrics)) {
-                                        df[,response$query$metrics[i]] <- as.numeric(df[,response$query$metrics[i]])
-                                        }
-                      
-                      ##CREATE DATE FRAME FOR START AND END DATE
-                      dates <- data.frame(
-                                         response$query['start-date'],
-                                         response$query['end-date'], 
-                                         stringsAsFactors=FALSE
-                                         )
-                      
-                      #CREATE DATA FRAME FOR PROFILE INFORMATION
-                      profileInfo <- data.frame(response$profileInfo, stringsAsFactors=FALSE)
-                      
-                      #FINAL DATA FRAME WITH DATE RANGE, PROFILE INFO AND RESULT ROWS
-                      finalDF <- cbind(profileInfo,dates, df)
-                      return(finalDF)
-}
+    
+    ##CREATE LIST OF QUERY PARAMETERS
+    queryList <- as.list(id)
+    for (x in 1:length(id)) { 
+      queryList[[x]] <- list(
+        'ids'=id[x],
+        'dimensions'=dimensions,
+        'metrics'=metrics,
+        'start-date'=start,
+        'end-date'=end,
+        'sort'=sort,
+        'max-results'=max,
+        'filters'=filters,
+        'access_token'=token
+      )
+    }
+    
+    ##REMOVE ANY UNUSED PARAMETERS
+    for (x in 1:length(id)){
+      queryList[[x]] <- queryList[[x]][!is.na(queryList[[x]])]
+    }
+    
+    ##BUILD QUERY AND FETCH RESULTS
+    response <- as.list(id)
+    for (x in 1:length(id)) {
+      response[[x]] <- GET("https://www.googleapis.com/analytics/v3/data/ga",
+                           query = queryList[[x]]
+      )
+    }
+    
+    #PARSE JSON RESPONSE FROM GA
+    for (x in 1:length(id)) {
+      response[[x]] <- jsonlite::toJSON(content(response[[x]]))
+      response[[x]] <- jsonlite::fromJSON(response[[x]], flatten=TRUE)
+    }
+    
+    ##ERROR HANDLING
+    
+    if(TRUE %in% grepl('error', response)){
+      ##IF ERROR THEN DATA FRAME OF ERROR RESPONSE CODE AND MESSAGE
+      
+      df <- list()
+      for (x in 1: length(id)){
+        df[x] <- list(data.frame(response[[x]]))
+      }
+      
+      df <- do.call('rbind',df)
+      
+    } else { 
+      ## IF NO ERROR, BUILD DATA FRAME OF RESPONSE DATA
+      
+      df <- list()
+      for (x in 1:length(id)) {
+        df[x] <- list(cbind(
+          data.frame(response[[x]]$profileInfo, stringsAsFactors=FALSE),
+          data.frame(response[[x]]$query['start-date'], stringsAsFactors=FALSE),
+          data.frame(response[[x]]$query['end-date'], stringsAsFactors=FALSE),
+          data.frame(response[[x]]$rows, stringsAsFactors=FALSE)
+        ))
+      }
+      
+      df <- do.call('rbind', df)
+      
+      ##RENAME COLUMNS
+      colnames(df) <- c(
+        names(response[[1]]$profileInfo),
+        names(response[[1]]$query['start-date']),
+        names(response[[1]]$query['end-date']),
+        unlist(response[[1]]$columnHeaders$name)
+      )
+      
+      #FORMAT METRIC DATA AS NUMERIC
+      metricNames <- as.vector(response[[1]]$query$metrics)
+      for (i in metricNames){
+        df[,i] <- as.numeric(df[,i])
+      }
+      
+      ##REMOVE GA FROM COLUMN NAMES
+      colnames(df) <- gsub('ga:','',colnames(df))
+      
+    } #END IF ELSE STATEMENT
+    
+    
+    return(df)
+  }
