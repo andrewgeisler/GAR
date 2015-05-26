32c5e9baf307fba1e2c1141659f65213b4a894ac
andrewgeisler <andrew.geisler@gmail.com>
2015-05-25 22:00:56 -0500

    Added pagination feature to gaRequest

--- R/gaRequest.R
+++ R/gaRequest.R
@@ -1,7 +1,63 @@
 gaRequest <-
-function(id, dimensions=NA, metrics, start, end, token=NA, sort=NA, max=NA, segment=NA, filters=NA) {
+function(id, dimensions=NA, metrics, start, end, token=NA, sort=NA, max=1000, segment=NA, filters=NA, allResults=FALSE) {
+   
+  ##INTERNAL FUNCTION BUILD AND FETCH --------------------------------------------------------------------------------------------------------
+  buildAndFetch <- function(q){
+    ##BUILD QUERY AND FETCH RESULTS
+    r <-list()
+    for (x in 1:length(q)) {
+      r[[x]] <- GET("https://www.googleapis.com/analytics/v3/data/ga",
+                    query = q[[x]]
+      )
+    }
     
-  ##PRIORITIZES SUPPLIED TOKEN FIRST. THEN CHECKS FOR ENV VARIABLE, THEN DEFAULTS TO NA.
+    #PARSE JSON RESPONSE FROM GA
+    for (y in 1:length(r)) {
+      r[[y]] <- jsonlite::toJSON(content(r[[y]]))
+      r[[y]] <- jsonlite::fromJSON(r[[y]], flatten=TRUE)
+    }
+    return(r)
+  }
+  
+  ##INTERNAL FUNCTION LIST TO DF --------------------------------------------------------------------------------------------------------
+  toDF <- function(r) {
+    
+    df <- list()
+    for (x in 1:length(r)) {
+      df[x] <- list(cbind(
+        data.frame(r[[x]]$profileInfo, stringsAsFactors=FALSE),
+        data.frame(r[[x]]$query['start-date'], stringsAsFactors=FALSE),
+        data.frame(r[[x]]$query['end-date'], stringsAsFactors=FALSE),
+        data.frame(r[[x]]$totalResults, stringsAsFactors=FALSE),
+        data.frame(r[[x]]$rows, stringsAsFactors=FALSE)
+      ))
+    }
+    
+    df <- do.call('rbind', df)
+    
+    ##RENAME COLUMNS
+    colnames(df) <- c(
+      names(r[[1]]$profileInfo),
+      names(r[[1]]$query['start-date']),
+      names(r[[1]]$query['end-date']),
+      'totalResults',
+      unlist(r[[1]]$columnHeaders$name)
+    )
+    
+    #FORMAT METRIC DATA AS NUMERIC
+    metricNames <- as.vector(r[[1]]$query$metrics)
+    for (i in metricNames){
+      df[,i] <- as.numeric(df[,i])
+    }
+    
+    ##REMOVE GA FROM COLUMN NAMES
+    colnames(df) <- gsub('ga:','',colnames(df))
+    
+    return(df)
+  }
+  
+  
+  ##PRIORITIZES SUPPLIED TOKEN FIRST. THEN CHECKS FOR ENV VARIABLE, THEN DEFAULTS TO NA. ------------------------------------------------------------
   if (!is.na(token)){
     token <- token
   } else if (exists('GAR_ACCESS_TOKEN', envir=envGAR)){
@@ -11,88 +67,98 @@ function(id, dimensions=NA, metrics, start, end, token=NA, sort=NA, max=NA, segm
   }
   
   
-    ##CREATE LIST OF QUERY PARAMETERS
-    queryList <- as.list(id)
-    for (x in 1:length(id)) { 
-      queryList[[x]] <- list(
-        'ids'=id[x],
-        'dimensions'=dimensions,
-        'metrics'=metrics,
-        'start-date'=start,
-        'end-date'=end,
-        'sort'=sort,
-        'max-results'=max,
-        'filters'=filters,
-        'segment'=segment,
-        'access_token'=token
-      )
+  ##CREATE LIST OF INITIAL QUERY PARAMETERS --------------------------------------------------------------------------------------------------------
+  queryList <- as.list(id)
+  for (x in 1:length(id)) { 
+    queryList[[x]] <- list(
+      'ids'=id[x],
+      'dimensions'=dimensions,
+      'metrics'=metrics,
+      'start-date'=start,
+      'end-date'=end,
+      'sort'=sort,
+      'max-results'=max,
+      'filters'=filters,
+      'segment'=segment,
+      'access_token'=token
+    )
+  }
+    
+  ##REMOVE ANY UNUSED PARAMETERS
+  for (x in 1:length(id)){
+    queryList[[x]] <- queryList[[x]][!is.na(queryList[[x]])]
+  }
+    
+  
+  ##BUILD AND FETCH INITIAL QUERY --------------------------------------------------------------------------------------------------------
+  finalDf <- buildAndFetch(queryList)
+  
+  
+  ###CHECK FOR INITIAL ERRORS
+  if(TRUE %in% grepl('error', finalDf)){
+    ##IF ERROR THEN DATA FRAME OF ERROR RESPONSE CODE AND MESSAGE
+    
+    finalDf <- list()
+    for (x in 1: length(id)){
+      finalDf[x] <- list(data.frame(finalDf[[x]]))
     }
     
-    ##REMOVE ANY UNUSED PARAMETERS
-    for (x in 1:length(id)){
-      queryList[[x]] <- queryList[[x]][!is.na(queryList[[x]])]
-    }
+    finalDf <- do.call('rbind',finalDf)
     
-    ##BUILD QUERY AND FETCH RESULTS
-    response <- as.list(id)
-    for (x in 1:length(id)) {
-      response[[x]] <- GET("https://www.googleapis.com/analytics/v3/data/ga",
-                           query = queryList[[x]]
-      )
-    }
-    
-    #PARSE JSON RESPONSE FROM GA
-    for (x in 1:length(id)) {
-      response[[x]] <- jsonlite::toJSON(content(response[[x]]))
-      response[[x]] <- jsonlite::fromJSON(response[[x]], flatten=TRUE)
-    }
-    
-    ##ERROR HANDLING
-    
-    if(TRUE %in% grepl('error', response)){
-      ##IF ERROR THEN DATA FRAME OF ERROR RESPONSE CODE AND MESSAGE
-      
-      df <- list()
-      for (x in 1: length(id)){
-        df[x] <- list(data.frame(response[[x]]))
-      }
-      
-      df <- do.call('rbind',df)
-      
-    } else { 
-      ## IF NO ERROR, BUILD DATA FRAME OF RESPONSE DATA
-      
-      df <- list()
-      for (x in 1:length(id)) {
-        df[x] <- list(cbind(
-          data.frame(response[[x]]$profileInfo, stringsAsFactors=FALSE),
-          data.frame(response[[x]]$query['start-date'], stringsAsFactors=FALSE),
-          data.frame(response[[x]]$query['end-date'], stringsAsFactors=FALSE),
-          data.frame(response[[x]]$rows, stringsAsFactors=FALSE)
-        ))
-      }
-      
-      df <- do.call('rbind', df)
-      
-      ##RENAME COLUMNS
-      colnames(df) <- c(
-        names(response[[1]]$profileInfo),
-        names(response[[1]]$query['start-date']),
-        names(response[[1]]$query['end-date']),
-        unlist(response[[1]]$columnHeaders$name)
-      )
-      
-      #FORMAT METRIC DATA AS NUMERIC
-      metricNames <- as.vector(response[[1]]$query$metrics)
-      for (i in metricNames){
-        df[,i] <- as.numeric(df[,i])
-      }
-      
-      ##REMOVE GA FROM COLUMN NAMES
-      colnames(df) <- gsub('ga:','',colnames(df))
-      
-    } #END IF ELSE STATEMENT
-    
-    
-    return(df)
+  } else {
+  
+  finalDf <- toDF(finalDf)
+  } #END IF ELSE STATEMENT
+  
+  
+  ##CREATE LIST OF PAGINATION QUERY PARAMETERS --------------------------------------------------------------------------------------------------------
+  if (allResults==TRUE & max==10000) {
+  
+  totalResults <- aggregate(data=finalDf, totalResults~tableId, FUN=mean)
+  totalResults$pages <- floor(totalResults$totalResults/max)
+  
+  tmp <- data.frame(profileId=NA, pages=NA)
+  for (x in totalResults$tableId){
+    tmp <- rbind(tmp,
+            data.frame(profileId=totalResults$tableId[totalResults$tableId==x], pages=0:totalResults$pages[totalResults$tableId==x], stringsAsFactors=FALSE, row.names=NULL)
+    )
+  }
+
+  tmp <- tmp[!is.na(tmp$pages) & tmp$pages>0,]
+  rm(totalResults)
+  
+  queryListPaginate <- list()
+  for (x in 1:length(tmp$profileId)) {
+    queryListPaginate[[x]] <- list(
+      'ids'=tmp$profileId[x],
+      'dimensions'=dimensions,
+      'metrics'=metrics,
+      'start-date'=start,
+      'end-date'=end,
+      'sort'=sort,
+      'max-results'=max,
+      'filters'=filters,
+      'segment'=segment,
+      'start-index'=(tmp$pages[x]*max)+1,
+      'access_token'=token
+    )
+  }
+  
+  ##REMOVE ANY UNUSED PARAMETERS
+  for (x in 1:length(queryListPaginate)){
+    queryListPaginate[[x]] <- queryListPaginate[[x]][!is.na(queryListPaginate[[x]])]
+  }
+  
+  ##BUILD AND FETCH INITIAL QUERY --------------------------------------------------------------------------------------------------------
+  paginateDf <- buildAndFetch(queryListPaginate)
+  paginateDf <- toDF(paginateDf)
+  
+  finalDf <- rbind(finalDf, paginateDf)
+  } else {
+  finalDf <- finalDf
+  }
+  
+  ##REMOVE UNNECESSARY COLUMNS FOR FINAL DF ------------------------------------------------------------------------------------------------
+  finalDf <- finalDf[,colnames(finalDf) != 'totalResults']
+  return(finalDf)
   }
