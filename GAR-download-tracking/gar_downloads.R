134d729e0c63e2771f408953df107ee66bfa42b4
andrewgeisler <andrew.geisler@gmail.com>
2015-06-08 20:45:16 -0500

    Added download tracking script

---
+++ GAR-download-tracking/gar_downloads.R
@@ -0,0 +1,13 @@
+# get the latest installr package:
+if (!require('devtools')) install.packages('devtools'); require('devtools')
+install_github('installr', 'talgalili')
+require(installr)
+
+
+RStudio_CRAN_data_folder <- download_RStudio_CRAN_data(START = '2015-03-20', END = Sys.Date()-1) # around the time R 3.0.0 was released
+
+my_RStudio_CRAN_data <- read_RStudio_CRAN_data(RStudio_CRAN_data_folder)
+
+
+barplot_package_users_per_day("GAR", my_RStudio_CRAN_data)
+
